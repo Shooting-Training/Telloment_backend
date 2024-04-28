70d71b3112463877eea7bda03947552f6fa552f1
advicewook <ceo0818@gmail.com>
2024-04-29 00:09:47 +0900

    기초 코드 2차

--- src/main/java/cau/capstone/backend/aws/S3Controller.java
+++ src/main/java/cau/capstone/backend/aws/S3Controller.java
@@ -1,51 +1,51 @@
-package cau.capstone.backend.aws;
-
-
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-
-@RequiredArgsConstructor
-@RestController
-public class S3Controller {
-
-    @Autowired
-    private S3Service s3Service;
-
-    @PostMapping("/s3/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        try {
-            String fileUrl = s3Service.saveFile(file);
-            return ResponseEntity.ok("File uploaded successfully. URL: " + fileUrl);
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Failed to upload file: " + e.getMessage());
-        }
-    }
-
-//    @GetMapping("/s3/download")
-//    public ResponseEntity<Resource> downloadFile(@RequestParam("fileName") String fileName) {
+//package cau.capstone.backend.aws;
+//
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.IOException;
+//
+//
+//@RequiredArgsConstructor
+//@RestController
+//public class S3Controller {
+//
+//    @Autowired
+//    private S3Service s3Service;
+//
+//    @PostMapping("/s3/upload")
+//    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
 //        try {
-//            ResponseEntity<UrlResource> response = s3Service.downloadImage(fileName);
-//            HttpHeaders headers = response.getHeaders();
-//            return ResponseEntity.ok()
-//                    .headers(headers)
-//                    .body(response.getBody());
-//        } catch (MalformedURLException e) {
+//            String fileUrl = s3Service.saveFile(file);
+//            return ResponseEntity.ok("File uploaded successfully. URL: " + fileUrl);
+//        } catch (IOException e) {
 //            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .body(new ByteArrayResource(("Failed to download file: " + e.getMessage()).getBytes()));
+//                    .body("Failed to upload file: " + e.getMessage());
 //        }
 //    }
-
-}
+//
+////    @GetMapping("/s3/download")
+////    public ResponseEntity<Resource> downloadFile(@RequestParam("fileName") String fileName) {
+////        try {
+////            ResponseEntity<UrlResource> response = s3Service.downloadImage(fileName);
+////            HttpHeaders headers = response.getHeaders();
+////            return ResponseEntity.ok()
+////                    .headers(headers)
+////                    .body(response.getBody());
+////        } catch (MalformedURLException e) {
+////            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+////                    .body(new ByteArrayResource(("Failed to download file: " + e.getMessage()).getBytes()));
+////        }
+////    }
+//
+//}
