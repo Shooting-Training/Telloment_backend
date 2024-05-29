f4548815e8442e0b32f88b69bb1eb1df9a42c842
박진영 <tear4321@cau.ac.kr>
2024-05-29 17:29:18 +0900

    Update Book.java

--- src/main/java/cau/capstone/backend/page/model/Book.java
+++ src/main/java/cau/capstone/backend/page/model/Book.java
@@ -25,6 +25,7 @@ public class Book extends BaseEntity {
     @Column(name = "book_id")
     private Long id;
 
+    @Column(name = "book_name")
     private String bookName;
 
     @ManyToOne(fetch = FetchType.LAZY)
